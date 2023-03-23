95719c938af2362150f42f69c1f68dbec0a5e0ab
Tanzil[laptop] <46586118+radio4444@users.noreply.github.com>
2023-03-23 10:38:16 -0400

    Merge to master

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 # this file is going development branch
 # Push this file while in development branch
+# In other words, the key difference between merge and rebase
+# is that while merge preserves history as it happened, rebase rewrites it
+# Merge this file to master branch
