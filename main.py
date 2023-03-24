7bfe43d0d6205ad0fcb2c76f6f04ab19f713b964
Tanzil[laptop] <46586118+radio4444@users.noreply.github.com>
2023-03-24 16:56:44 -0400

    Rename scratch(testing) to archive. Added Scratch(testing) branch

--- main.py
+++ main.py
@@ -30,4 +30,19 @@ morse_code = {
 	"Z": "--.."
 }
 
-# this is scratch. testing out how to use join method
+# Mini dictionary practice
+# Print format, and how to access the keys
+# print(f"{morse_code['A']} = {morse_code.get('A')}")
+# testing manually
+# print(f"A = {morse_code['A']}")
+# print(f"A = {morse_code.get('A')}, B = {morse_code['B']}")
+# for loop. From  A to H. 3 units space.
+# for keys in range(0, 5):
+# 	print(list(morse_code)[:keys])
+# print(list(morse_code)[:5])
+# for keys in morse_code:
+# 	print(f"{morse_code.get(keys)}")
+
+
+
+# this is scratch. How to combine the letter into one word
