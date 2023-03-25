28397608924f309fb8ee2c3dc65e0d98dfb3e1da
Tanzil[laptop] <46586118+radio4444@users.noreply.github.com>
2023-03-25 15:11:03 -0400

    Morse Code to English Conversion done.

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ morse_code_dict = {
 
 
 # Convert English sentence to Morse Code
-def morse_code_em(english: str) -> None:
+def morse_code_emf(english: str) -> None:
 	dummy_morse_code = ""
 	for cha in english:
 		for key in morse_code_dict:
@@ -43,12 +43,33 @@ def morse_code_em(english: str) -> None:
 		print(f"Original : {english} | Morse Code: {dummy_morse_code}")
 
 
+# Convert Morse code to English
+def morse_code_mef(morse_code: str) -> None:
+	dummy_english = ""
+	for index in morse_code.split(" "):  # morse_code.split(" ") is a list, where index iterate through it
+		for key_index in morse_code_dict.keys():
+			if index == morse_code_dict.get(key_index):
+				dummy_english += key_index
+
+	print(f"Original : {morse_code} | English: {dummy_english}")
+
+
 # Input from the user:
 while True:
 	sentence = input("Input the message: ")
-	morse_code_em(sentence)
+	# morse_code_emf(sentence)
+	morse_code_mef(sentence)
 	if sentence == "":
 		break
 
-
 # this is MorseC_English(testing) branch
+morse_code_mef(". .... ... .- -.")
+morse_code_mef(".... .")  # output HE
+# how to iterate through string, where space it will stop
+string = ". ...."
+string_list = []
+for cha in range(0, len(string)):
+	if string[cha] != " ":
+		string_list += string[cha]
+# print("working cha")
+print(string.split(" "))
