d7eb7ed8e602c5e169c51533e39e67eb955a3f78
Tanzil[laptop] <46586118+radio4444@users.noreply.github.com>
2023-03-31 17:41:29 -0400

    Update morse_code_dict: \n. Update file function: iterate through the file line by line

--- encode_decode_file/encode_decode.py
+++ encode_decode_file/encode_decode.py
@@ -26,7 +26,8 @@ morse_code_dict = {
 	"Y": "-.--",
 	"Z": "--..",
 	" ": "/",
-	".": ".-.-.-"
+	".": ".-.-.-",
+	"\n": "\n"  # Create new line in the morse code.
 }
 
 
@@ -52,10 +53,7 @@ def morse_code_mef(morse_code: str) -> str:  # Convert Morse code to English
 	return dummy_english
 
 
-# Input and output file function.
-
-with open("Original message.txt", 'r') as r_file:
-	# Keep reading the file till there is none?
-	sentence = r_file.readline()
-	with open("Convert message.txt", 'w') as w_file:
+# Open the input file in read mode and output file in write mode
+with open("Original message.txt", 'r') as r_file, open("Convert message.txt", 'w') as w_file:
+	for sentence in r_file:  # iterate through the file line by line
 		w_file.write(str(morse_code_mef(sentence)))
