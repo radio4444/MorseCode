dc39f5b6b59e72fb2e06d72328b4fccdec5c4a84
Tanzil[laptop] <46586118+radio4444@users.noreply.github.com>
2023-03-31 16:26:07 -0400

    Function return string. those function able to print in new file

--- encode_decode_file/encode_decode.py
+++ encode_decode_file/encode_decode.py
@@ -30,8 +30,7 @@ morse_code_dict = {
 }
 
 
-# Convert English sentence to Morse Code
-def morse_code_emf(english: str) -> None:
+def morse_code_emf(english: str) -> str:  # Convert English sentence to Morse Code
 	dummy_morse_code = ""
 	for cha in english:
 		for key in morse_code_dict:
@@ -39,10 +38,10 @@ def morse_code_emf(english: str) -> None:
 				dummy_morse_code += morse_code_dict.get(key) + "   "
 	if english != "":  # you could comment this out. Not really needed.
 		print(f"Original : {english} | Morse Code: {dummy_morse_code}")
+		return dummy_morse_code
 
 
-# Convert Morse code to English
-def morse_code_mef(morse_code: str) -> None:
+def morse_code_mef(morse_code: str) -> str:  # Convert Morse code to English
 	dummy_english = ""
 	for index in morse_code.split(" "):  # morse_code.split(" ") is a list, where index iterate through it
 		for key_index in morse_code_dict.keys():
@@ -50,19 +49,13 @@ def morse_code_mef(morse_code: str) -> None:
 				dummy_english += key_index
 
 	print(f"Original : {morse_code} | English: {dummy_english}")
+	return dummy_english
 
 
-# Create a file called "original message" [manually]
-# -content: this is a sentence. | This is another sentence.
+# Input and output file function.
 
-with open("Original message.txt", 'r') as r_file:  # after 'as', we declare variable to call the 'file'
-	# Create a variable called "sentence", which store the content from the "Original message"
-	sentence = r_file.read()
-	# Create a new filed called: "Encode/decode message" and output the content from the "sentence"
-	# cannot use / as it mean to be use for directory.
+with open("Original message.txt", 'r') as r_file:
+	# Keep reading the file till there is none?
+	sentence = r_file.readline()
 	with open("Convert message.txt", 'w') as w_file:
-		w_file.write(str(sentence))
-
-# Two ways we could create stop.
-	# Either using split method
-	# Or if cha == '.' then
+		w_file.write(str(morse_code_mef(sentence)))
