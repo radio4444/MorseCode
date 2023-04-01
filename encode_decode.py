102fd07b7687aad2c0b11c52f74706cd9ba211fc
Tanzil[laptop] <46586118+radio4444@users.noreply.github.com>
2023-04-01 15:26:47 -0400

    Update input user. (Input file, does not take any file) Update r_file and w_file  (strip) encode_decode.py

--- encode_decode.py
+++ encode_decode.py
@@ -27,11 +27,10 @@ morse_code_dict = {
 	"Z": "--..",
 	" ": "/",
 	".": ".-.-.-",
-	"\n": "\n"  # Create new line in the morse code.
 }
 
 
-def morse_code_emf(english: str) -> str:  # Convert English sentence to Morse Code
+def morseCode_EM(english: str) -> str:  # Convert English sentence to Morse Code
 	dummy_morse_code = ""
 	for cha in english:
 		for key in morse_code_dict:
@@ -42,7 +41,7 @@ def morse_code_emf(english: str) -> str:  # Convert English sentence to Morse Co
 		return dummy_morse_code
 
 
-def morse_code_mef(morse_code: str) -> str:  # Convert Morse code to English
+def morseCode_ME(morse_code: str) -> str:  # Convert Morse code to English
 	dummy_english = ""
 	for index in morse_code.split(" "):  # morse_code.split(" ") is a list, where index iterate through it
 		for key_index in morse_code_dict.keys():
@@ -54,7 +53,13 @@ def morse_code_mef(morse_code: str) -> str:  # Convert Morse code to English
 
 
 # Open the input file in read mode and output file in write mode
-# Use it for reference, later delete
-# with open("Original message.txt", 'r') as r_file, open("Convert message.txt", 'w') as w_file:
+with open("Morse message.txt", 'r') as r_file, open("Convert message.txt", 'w') as w_file:
+	for sentence in r_file:  # iterate through the file line by line
+		sentence = sentence.strip()
+		w_file.write(morseCode_ME(sentence).strip())
+		w_file.write("\n")
+# with open("English message.txt", 'r') as r_file, open("Convert message.txt", 'w') as w_file:
 # 	for sentence in r_file:  # iterate through the file line by line
-# 		w_file.write(str(morse_code_mef(sentence)))
+# 		sentence = sentence.strip()
+# 		w_file.write(morseCode_EM(sentence).strip())
+# 		w_file.write("\n")
--- main.py
+++ main.py
@@ -1,30 +1,46 @@
 # Credit to https://morsecode.world/international/translator.html
 # Using this website, to test my code. XD
-from encode_decode_file import encode_decode as ed
+import encode_decode as ed
 
 # Input from the user:
 while True:  # Loop till no input message
     choice_SorF = input("Write message(W) or Input file(I): ").upper()
     if choice_SorF == "Write message" or choice_SorF == "W":
         sentence = input("Press enter to exit or Input message: ")
+        # Later: input will be able to detect me or em without user specifying it.
         while sentence != "":
             choice_EorM = input("Type English to Morse(EM) or Morse to English(ME): ").upper()
-            # Later: input will be able to detect me or em without user specifying it.
             if choice_EorM == "ME" or choice_EorM == "Morse to English":
-                ed.morse_code_mef(sentence)
+                ed.morseCode_ME(sentence)
                 break
             elif choice_EorM == "EM" or choice_EorM == "English to Morse":
-                ed.morse_code_emf(sentence)
+                ed.morseCode_EM(sentence)
                 break
             else:  # Ask the user again, if they didn't enter either.
                 print("Please type EM or ME")
         else:  # Prompt (Write message or Input file) again
             print("Thank you for using \"write message.\"")
     elif choice_SorF == "Input file" or choice_SorF == "I":
-        print("Yes you are at file")
-        with open("Original message.txt", 'r') as r_file, open("Convert message.txt", 'w') as w_file:
-            for sentence in r_file:  # iterate through the file line by line
-                w_file.write(str(ed.morse_code_mef(sentence)))
+        file = input("Press enter to exit or Input file: ")
+        # Later: input will be able to detect me or em without user specifying it.
+        while file != "":
+            choice_EorM = input("Type English to Morse(EM) or Morse to English(ME): ").upper()
+            if choice_EorM == "ME" or choice_EorM == "Morse to English":
+                with open("English message.txt", 'r') as r_file, open("Convert message.txt", 'w') as w_file:
+                    for sentence in r_file:  # iterate through the file line by line
+                        sentence = sentence.strip()
+                        w_file.write(ed.morseCode_EM(sentence).strip())
+                        w_file.write("\n")
+                break
+            elif choice_EorM == "EM" or choice_EorM == "English to Morse":
+                with open("English message.txt", 'r') as r_file, open("Convert message.txt", 'w') as w_file:
+                    for sentence in r_file:  # iterate through the file line by line
+                        sentence = sentence.strip()
+                        w_file.write(ed.morseCode_EM(sentence).strip())
+                        w_file.write("\n")
+                break
+            else:  # Ask the user again, if they didn't enter either.
+                print("Please type EM or ME")
     else:  # End the program.
         print("Thank you for using \"morse program.\"")
         break
