6223cf27c737191befae137db4da0e222acd910c
Tanzil[laptop] <46586118+radio4444@users.noreply.github.com>
2023-04-02 13:23:22 -0400

    Update: User able to output filename (w_file) Update: User does not need to type .txt (r_file) Update: Does not prompt w_file, when the r_file is empty. Straight to choice_SorF prompt

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # Credit to https://morsecode.world/international/translator.html
 # Using this website, to test my code. XD
 import encode_decode as ed
+import os
+
+cwd = os.getcwd()  # get current directory
+
 
 # Input from the user:
 while True:  # Loop till no input message
@@ -21,19 +25,22 @@ while True:  # Loop till no input message
         else:  # Prompt (Write message or Input file) again
             print("Thank you for using \"write message.\"")
     elif choice_SorF == "Input file" or choice_SorF == "I":
-        file = input("Press enter to exit or Input filename: ")
+        r_file = input("Press enter to exit or Input the filename: ")
+        r_filepath = os.path.join(cwd, 'encode_decode_file', r_file+'.txt') # reading file from encode_decode_file directory
         # Later: input will be able to detect me or em without user specifying it.
-        while file != "":
+        while r_file != "":
+            w_file = input("Output the filename: ")
+            w_filepath = os.path.join(cwd, 'encode_decode_file', w_file + '.txt')
             choice_EorM = input("Type English to Morse(EM) or Morse to English(ME): ").upper()
             if choice_EorM == "ME" or choice_EorM == "Morse to English":
-                with open(file, 'r') as r_file, open("Convert message.txt", 'w') as w_file:
+                with open(r_filepath, 'r') as r_file, open(w_filepath, 'w') as w_file:
                     for sentence in r_file:  # iterate through the file line by line
                         sentence = sentence.strip()  # for the print message.
                         w_file.write(ed.morseCode_ME(sentence).strip())  # for the output file message.
                         w_file.write("\n")
                 break
             elif choice_EorM == "EM" or choice_EorM == "English to Morse":
-                with open(file, 'r') as r_file, open("Convert message.txt", 'w') as w_file:
+                with open(r_filepath, 'r') as r_file, open(w_filepath, 'w') as w_file:
                     for sentence in r_file:  # iterate through the file line by line
                         sentence = sentence.strip()  # for the print message.
                         w_file.write(ed.morseCode_EM(sentence).strip())  # for the output file message.
